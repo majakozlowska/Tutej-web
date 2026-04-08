727ff2f39eca287ad402761384368e9178bafbb0
Maja <maja07k@gmail.com>
2026-04-08 19:57:39 +0200

    initialize docker compose and database schema

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Backend działa!');
+});
+
+app.listen(port, () => {
+    console.log(`Serwer na porcie ${port}`);
+});
