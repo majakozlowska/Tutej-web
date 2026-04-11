7d23a05ea384602a7b926b10a90e12edc789797d
Maja <maja07k@gmail.com>
2026-04-11 13:48:03 +0200

    Add Text component and adjust login styles

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,12 +1,17 @@
-import { Heading } from "../../components/Heading";
 import style from "./login.module.scss";
+import { Heading } from "../../components/Heading";
+import { Text } from "../../components/Text";
 
 export function Login() {
     return (
-        <div className={style.container}>
+        <div className={style.mainContainer}>
             <img src="/illustrations/01.svg"  alt="Login illustration" className={style.illustration} />
-            <div className={style.form}>
+            <div className={style.container}>
                 <Heading text="Witamy ponownie!"/>
+                <div className={style.form}>
+                    <Text text="Twoje dane" />
+                </div>
+                <Text text="Nie posiadasz jeszcze konta?" />
             </div>
         </div>
     );
