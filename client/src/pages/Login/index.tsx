e99a268b24ec89a7d05be11399f60ecd64b9b293
Maja <maja07k@gmail.com>
2026-04-11 13:29:58 +0200

    Add illustration

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -4,7 +4,10 @@ import style from "./login.module.scss";
 export function Login() {
     return (
         <div className={style.container}>
-            <Heading text="Witamy ponownie!"/>
+            <img src="/illustrations/01.svg"  alt="Login illustration" className={style.illustration} />
+            <div className={style.form}>
+                <Heading text="Witamy ponownie!"/>
+            </div>
         </div>
     );
 }
